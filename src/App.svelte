4daca7f3a8c3dd824027d10c33a12d82f353789d
Kaoruko <yykaoruko@gmail.com>
2021-05-11 22:46:50 +0900

    wip

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Router from 'svelte-spa-router';
+	import Home from './routes/Home.svelte';
+	import Detail from './routes/Detail.svelte';
+	import New from './routes/New.svelte';
+
+	const routes = {
+		'/': Home,
+		'/todos/detail': Detail,
+		'/todos/new': New,
+	};
+</script>
+
+<main>
+  <Router {routes}/>
+</main>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { todos } from '../store';
+  import { push, link } from 'svelte-spa-router';
+
+  const handleOnClickDelete = (index) => {
+    $todos.splice(index, 1);
+    $todos = [...$todos];
+  }
+
+  const handleOnClickEdit = (index) => {
+    push(`/todos/${index}`);
+  }
+</script>
+
+
+<h1>Todo</h1>
+<a use:link href="/todos/new">new</a>
+
+<table>
+  {#each $todos as todo, index}
+  <tr>
+    <td>{todo.text}</td>
+    <td>
+      <button on:click={() => handleOnClickEdit(index)}>edit</button>
+      <button on:click={() => handleOnClickDelete(index)}>delete</button>
+    </td>
+  </tr>
+  {/each}
+</table>
